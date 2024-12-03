6185fc22129417e5bfdda1d2f141534614b7795e
Maxwell Rodriguez <maxbad23@gmail.com>
2024-12-03 12:02:17 +0100

    Added unlock and lock of slcr registers and cache flush/invalidation for succesful R/W operations on OCM

--- OCM_test/src/ocm_test.c
+++ OCM_test/src/ocm_test.c
@@ -1,7 +1,7 @@
 /*
 *
-* Simple OCM test that periodically reads an OCM memory address and change gpio state according to the value on that address
-* Core 0 is responsible for changing the value -> not reached yet
+* Simple OCM test that relocates OCM address to high address config by unlock and locking slcr registers, for R/W operations on OCM memory.
+* Task1 = succesfully reads OCM base address
 *
 */
 
@@ -31,8 +31,14 @@ int LED1 = 0x1; // LED1 mask
 
 /*---------------------- OCM Parameters---------------------*/
 #define TARGET_ADDR 0xFFFC0000 // Dirección prueba comm OCM
-#define OCM_CFG_ADDR 0x0000001F // OCM_CFG direction
+#define OCM_CFG_ADDR 0xF8000910 // OCM_CFG direction
 #define ADDR_OFFSET (*(volatile uint32_t *)(0x45))          // Offset for OCM adddr(offset de 69 -> 0xFFFC0045)
+
+#define SLCR_UNLOCK_ADDR 0xF8000008
+#define SLCR_LOCK_ADDR   0xF8000004
+#define UNLOCK_KEY       0xDF0D
+#define LOCK_KEY         0x767B
+
 /*-----------------------------------------------------------*/
 
 
@@ -59,18 +65,28 @@ int main( void ){
 
     int status = 0;
     XGpio_Config *CfgPtr;
-    volatile uint32_t *OCM_CFG = (uint32_t *)OCM_CFG_ADDR;
+   
+     
+    Xil_Out32(SLCR_UNLOCK_ADDR, UNLOCK_KEY);  // Desbloquea registros SLCR
+    Xil_Out32(OCM_CFG_ADDR, 0x0000001F);     // Escribe en OCM_CFG
+    Xil_Out32(SLCR_LOCK_ADDR, LOCK_KEY);     // Bloquea registros SLCR
+    Xil_DCacheFlushRange((INTPTR)OCM_CFG_ADDR, sizeof(uint32_t));  // Sincroniza cache con memoria
 
-    
-    *OCM_CFG = 0x0000001F; // OCM relocation enabled
-    Xil_DCacheFlush();
-    //Xil_Out32(0xF8000910, 0x0000001F); // OCM relocation -> high address config
-    printf("Direccion del puntero de OCM config %08x\r\n", &OCM_CFG);
-    printf("Valor del puntero en OCM_CFG: %08x\r\n", (*OCM_CFG));
-    printf("Valor con Xil_in32 del OCM_CFG: %08x\r\n", Xil_In32(OCM_CFG_ADDR));
+    // Lee el valor directamente del hardware
+    Xil_DCacheInvalidateRange((INTPTR)OCM_CFG_ADDR, sizeof(uint32_t));  // Invalida la cache antes de leer
+    uint32_t valor_directo = Xil_In32(OCM_CFG_ADDR);
+
+    // Valor a traves de puntero
+    volatile uint32_t *OCM_CFG = (uint32_t *)OCM_CFG_ADDR;
+    uint32_t valor_puntero = *OCM_CFG;
+
+    printf("Direccion de OCM config (puntero apunta a): %08x\r\n", OCM_CFG);
+    printf("Valor leido directamente con Xil_In32: %08x\r\n", valor_directo);
+    printf("Valor leido a través del puntero: %08x\r\n", valor_puntero);
+
     xil_printf("MAIN of GPIO test. Starting GPIO config...\r\n");
 
-    Xil_SetTlbAttributes(0xFFFC0000,0x14de2); // descached OCM space
+    //Xil_SetTlbAttributes(0xFFFC0000,0x14de2); // descached OCM space
 
     // GPIO config leds
     CfgPtr = XGpio_LookupConfig(XGPIO_AXI_LED_BASEADDR);
@@ -123,12 +139,12 @@ int main( void ){
 					&xThread1 );
 
         /* Task Creation */
-/* 	xTaskCreate( 	checkBTNS_Task, 					// The function that implements the task. 
-					( const char * ) "Thread1", 		// Text name for the task, provided to assist debugging only. 
+	xTaskCreate( 	checkBTNS_Task, 					// The function that implements the task. 
+					( const char * ) "Thread2", 		// Text name for the task, provided to assist debugging only. 
 					configMINIMAL_STACK_SIZE, 	// The stack allocated to the task. 
 					NULL, 						// The task parameter is not used, so set to NULL. 
-					tskIDLE_PRIORITY,		// The task runs at the idle priority + 1 (higher priority than idle). 
-					&xThread1 ); */
+					tskIDLE_PRIORITY + 1,		// The task runs at the idle priority + 1 (higher priority than idle). 
+					&xThread2 );
 
 
 	/* Start the tasks and timer running. */
@@ -150,18 +166,16 @@ static void checkOCM_Task( void *pvParameters ){
     //addrValue = 0x0000000F;
     while(1){
 
+    
+        Xil_DCacheInvalidateRange((INTPTR)TARGET_ADDR, sizeof(uint32_t));
         addrValue = Xil_In32(TARGET_ADDR);
-        //addrValue = TARGET_ADDR;
         xil_printf("Addr Value: %08x\r\n", addrValue);
-        // 0xE92D00F0 
-        if(addrValue > 0){ // any of 4 low bits enabled -> led enable. Old condition: (addrValue & (0x0000000F)) != 0
-            //ledValue = 0x0F;
-            //XGpio_DiscreteWrite(&Gpio, CH1, ledValue);
-            //Xil_Out32((TARGET_ADDR + ADDR_OFFSET) , 0x000000FF);
-            
-            xil_printf("Holaaaaa\r\n");
+
+        if(addrValue != 0xFFFF0000){ // != auxValue in checkBTNS_Task
+            Xil_Out32(TARGET_ADDR , 0xFFFF0000);
+            Xil_DCacheFlushRange((INTPTR)TARGET_ADDR, sizeof(uint32_t));
+            xil_printf("Writing ok\r\n");
         }
-        XGpio_DiscreteWrite(&Gpio, CH1, ledValue); 
         vTaskDelay(x500ms);
     }
 } 
@@ -176,8 +190,9 @@ static void checkBTNS_Task( void *pvParameters ){
     uint32_t auxValue = 0xFFFF0000; // specified value for turn led off without button
 
     while(1){
-        
-        //auxRead = Xil_In32(TARGET_ADDR);
+
+        Xil_DCacheInvalidateRange((INTPTR)TARGET_ADDR, sizeof(uint32_t));
+        auxRead = Xil_In32(TARGET_ADDR);
         swState = XGpio_DiscreteRead(&Gpio, CH2);
         btnState = XGpio_DiscreteRead(&Gpio_btns, CH1);
 
@@ -188,12 +203,17 @@ static void checkBTNS_Task( void *pvParameters ){
         }
 
         XGpio_DiscreteWrite(&Gpio, CH1, ledValue);
-        vTaskDelay(x100ms);
-
-/*         if((auxRead == auxValue) ){
-            //XGpio_DiscreteWrite(&Gpio_btns, CH1, ledValue);
+        if(auxRead == auxValue){
+            
             Xil_Out32((TARGET_ADDR + ADDR_OFFSET) , 0x00000000);
-        } */
+            Xil_DCacheFlushRange((INTPTR)(TARGET_ADDR + ADDR_OFFSET), sizeof(uint32_t));  // Sincroniza cache con memoria
+
+            Xil_DCacheInvalidateRange((INTPTR)(TARGET_ADDR + ADDR_OFFSET), sizeof(uint32_t));
+            auxRead = Xil_In32((TARGET_ADDR + ADDR_OFFSET));
+
+            xil_printf("TARGET + offset Value: %08x\r\n", auxRead);
+        }
+        vTaskDelay(x100ms);
         
     }
 }
